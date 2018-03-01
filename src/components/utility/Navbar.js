29c186002d6f6574858943f9bdf7c363f832bf1e
Amadea Kimmins <amadea.kimmins@hotmail.co.uk>
2018-03-01 16:08:36 +0000

    navbar change

--- src/components/utility/Navbar.js
+++ src/components/utility/Navbar.js
@@ -13,15 +13,15 @@ const Navbar1 = ({ history }) => {
   return (
     <div className="container">
       <nav className="navbar">
-        {!Auth.isAuthenticated() && <Link to="/login" className="navbar-item"><i className="fa fa-sign-in"></i> Login</Link>}
-        {' '}
-        { !Auth.isAuthenticated() && <Link to="/register" className="navbar-item">Register</Link>}
-        {' '}
         <Link to="/" className="navbar-item"><i className="fa fa-home"></i> Home</Link>
         {' '}
         <Link to="/brands" className="navbar-item"><i className="fa fa-shopping-bag"></i> Brands</Link>
         {' '}
         { !!Auth.isAuthenticated() && <Link to={'/users/' + Auth.getPayload().userId} className="navbar-item"><i className="fa fa-user"></i> Account</Link>}
+        {' '}
+        {!Auth.isAuthenticated() && <Link to="/login" className="navbar-item"><i className="fa fa-sign-in"></i> Login</Link>}
+        {' '}
+        { !Auth.isAuthenticated() && <Link to="/register" className="navbar-item">Register</Link>}
         {' '}
         { !!Auth.isAuthenticated() && <a href="#" className="navbar-item" onClick={logout}><i className="fa fa-sign-out"></i>Logout</a>}
       </nav>
